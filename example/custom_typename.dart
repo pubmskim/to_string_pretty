9d6145092a0a9451ea07eaafc0e4ebaa531f0a4a
Minsik Kim <64628625+pubmskim@users.noreply.github.com>
2021-02-24 09:48:15 +0900

    Adds the example for customizing typename.

---
+++ example/custom_typename.dart
@@ -0,0 +1,15 @@
+import 'package:to_string_pretty/to_string_pretty.dart';
+
+void main() {
+  final json = <String, dynamic>{
+    'url': 'https://pub.dev',
+    'packages': [
+      'shared_preferences',
+      'flutter_slidable',
+    ],
+    'datetime': DateTime.parse('2021-02-18T19:51:57.4139787-07:00'),
+    'fieldCount': 4,
+  };
+
+  print(toStringPretty('JsonType', json));
+}
